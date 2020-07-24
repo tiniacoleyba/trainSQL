5fdf94a30a0af54185468284821ebd925e8cbcff
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-07-24 21:16:42 +0100

    Occupations hackerrank second solution was added.

--- hackerrank/occupations_pivot_awesome_example.sql
+++ hackerrank/occupations_pivot_awesome_example.sql
@@ -1,5 +1,7 @@
 -- https://www.hackerrank.com/challenges/occupations/problem
 -- Oracle
+
+-- Solution 1: multiple joins
 with o as (
     select
         name,
@@ -35,3 +37,22 @@ left join
 on
     orn.rn = oa.rn and
     oa.occupation = 'Actor';
+
+-- Solution 2: group by and decode
+select
+    min(decode(occupation,'Doctor',name)),
+    min(decode(occupation,'Professor',name)),
+    min(decode(occupation,'Singer',name)),
+    min(decode(occupation,'Actor',name))
+from
+     (select
+        name,
+        occupation,
+        row_number() over(partition by occupation order by name) as rn
+      from
+        occupations
+     ) o
+group by
+    rn
+order by
+    rn;
