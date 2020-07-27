47b15e5b13b32e9eeed12e30843a47041157237d
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-07-27 06:41:08 +0100

    Added cast example.

---
+++ dvdrental/cast.sql
@@ -0,0 +1,15 @@
+-- PostgreSQL
+select
+    cast('10' as integer),
+    cast('2020-07-30' as date),
+    cast('2017-01-28 16:00:00' as timestamp),
+    cast('T' as boolean),
+    cast('F' as boolean),
+    cast('true' as boolean),
+    cast('false' as boolean),
+    cast('3.21' as double precision),
+    cast('15 min' as interval),
+    cast('2 hour' as interval),
+    cast('1 day' as interval),
+    cast('1 week' as interval),
+    cast('1 month' as interval)
