fee072980d00b81278d9a10ceb2ab3525e7a3dc7
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-08-01 09:18:15 +0100

    Added contest leaderboard query solution.

--- hackerrank/contest_leaderboard.sql
+++ hackerrank/contest_leaderboard.sql
@@ -1,6 +1,6 @@
 -- https://www.hackerrank.com/challenges/contest-leaderboard/problem
 
--- Oracle
+-- Oracle. Solution using row_number analytic function.
 select
     i.hacker_id,
     h.name,
@@ -27,4 +27,33 @@ having
     sum(i.score) > 0
 order by
     sum(i.score) desc,
+    i.hacker_id;
+
+-- MySQL. Solution using group by.
+select
+    i.hacker_id,
+    h.name,
+    sum(i.max_score)
+from (
+    select
+        hacker_id,
+        challenge_id,
+        max(score) as max_score
+    from
+        submissions s
+    group by
+        hacker_id,
+        challenge_id
+) i
+inner join
+    hackers h
+on
+    i.hacker_id = h.hacker_id
+group by
+    i.hacker_id,
+    h.name
+having
+    sum(i.max_score) > 0
+order by
+    sum(i.max_score) desc,
     i.hacker_id;
