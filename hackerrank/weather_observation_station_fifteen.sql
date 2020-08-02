f9f335ec3d51abbc26043f37e78d770d8c79874c
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-08-02 07:09:25 +0100

    Added weather observation station fifteen query solution.

---
+++ hackerrank/weather_observation_station_fifteen.sql
@@ -0,0 +1,11 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-15/problem
+
+-- Oracle, MySQL.
+select
+    round(long_w,4)
+from
+    station
+where
+    lat_n = (select max(lat_n)
+             from station
+             where lat_n < 137.2345);
